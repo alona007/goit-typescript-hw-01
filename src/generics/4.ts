6211cc377edb4287d441eb9d825814f13362ac25
alona007 <alona.olyksashenko@gmail.com>
2024-08-17 12:36:01 +0200

    start

---
+++ src/generics/4.ts
@@ -0,0 +1,27 @@
+type User = {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+};
+
+const userInfo: User = {
+  name: "Kateryna",
+  surname: "Kononova",
+  email: "kononova@gmail.com",
+  password: "1234",
+};
+
+function createOrUpdateUser(initialValues: User, updateUser: Partial<User>) {
+  return {
+    ...initialValues,
+    ...updateUser,
+  };
+}
+
+console.log(
+  createOrUpdateUser(userInfo, {
+    email: "user@mail.com",
+    password: "password123",
+  })
+);
